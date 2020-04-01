7bc9c880fb7b37899c98baa592610c264aff18b4
JUAN DAVID OCHOA GAVIRIA <juanochoa220427@correo.itm.edu.co>
2020-03-31 21:11:41 -0500

    Completed Seventh Step from Fourth Section - Add autentication, Rendering conditional and implements routing libraries.

--- src/Components/todo/TodoApp.jsx
+++ src/Components/todo/TodoApp.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class TodoApp extends Component {
     render(){
         return (
             <div className="todoApp">
-                <LoginComponent/>
+                <Router>
+                    <Route path="/" exact component={LoginComponent}/>
+                    <Route path="/Login" component={LoginComponent}/>
+                    <Route path="/Welcome" component={WelcomeComponent}/>
+                </Router>
+                    {/*<LoginComponent/>
+                    <WelcomeComponent/>*/}
             </div>
         )
     }
@@ -15,11 +22,14 @@ class LoginComponent extends Component{
         super(props);
         this.state = {
             username: 'jochoaTest',
-            password : ''
+            password : '',
+            hasLoginFailed : false,
+            showSucessMessage : false
         }
         // this.handleUserNameChange = this.handleUserNameChange.bind(this)
         // this.handlePasswordChange = this.handlePasswordChange.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
     }
 
 
@@ -44,17 +54,50 @@ class LoginComponent extends Component{
     //     this.setState({password:event.target.value})
     // }
 
+    loginClicked(){
+        //jochoaTest && abcd
+        if(this.state.username==="jochoaTest" && this.state.password==="abcd"){
+            console.log('Sucessful')
+            this.setState({showSucessMessage:true, hasLoginFailed:false});
+        }else{
+            console.log('Failed')
+            this.setState({hasLoginFailed:true, showSucessMessage:false});
+        }
+    }
+
     render(){
         return(
             <div className="loginComponent"><br/>
+                {/*<ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
+                <ShowLoginSucessful showSucessMessage={this.state.showSucessMessage}/>*/}
+                {this.state.hasLoginFailed && <div>Invalid Credentials</div>} {/*This line remplace the component function*/}
+                {this.state.showSucessMessage && <div>Login Sucessful</div>} 
                 UserName: <input type="text" name="username" required value={this.state.username} onChange={this.handleChange}/>
                 Password: <input type="password" name="password" required value={this.state.password} onChange={this.handleChange}/>
                 <div className="loginButton">
-                    <br/><button>Login</button>
+                    <br/><button onClick={this.loginClicked}>Login</button>
                 </div>
             </div>
         )
     }
 }
+
+class WelcomeComponent extends Component{
+    render(){
+        return <div>Welcome - Juan</div>
+    }
+}
+
+// function ShowInvalidCredentials(props){
+//     if(props.hasLoginFailed)
+//         return  <div>Invalid Credentials</div> 
+//     return null
+// }
+
+// function ShowLoginSucessful(props){
+//     if(props.showSucessMessage)
+//         return <div>Login Sucessful</div>
+//     return null
+// }
 
 export default TodoApp
