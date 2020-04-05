fd49b13e948c652f415961a69696f3ae27f8b763
JUAN DAVID OCHOA GAVIRIA <juanochoa220427@correo.itm.edu.co>
2020-04-05 18:37:26 -0500

    Completed the section 5 - First steps with RESTful WS Spring boot

---
+++ restful-web-services/src/main/java/com/jochoatest/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,22 @@
+package com.jochoatest.restfulwebservices;
+
+public class HelloWorldBean {
+	private String message;
+	
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Hello World Bean [message=%s]", message);
+	}
+}
