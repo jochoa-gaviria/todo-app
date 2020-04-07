e318cc75c2e71bbdb30b8d12b02dcaaf36601f8a
JUAN DAVID OCHOA GAVIRIA <juanochoa220427@correo.itm.edu.co>
2020-04-06 20:19:54 -0500

    add CrossOrigin from localhost:4200

--- restful-web-services/src/main/java/com/jochoatest/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/jochoatest/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.jochoatest.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200") //It's to solve error for access CORS 'Access-Control-Allow-Origin
 public class HelloWorldController {
 
 	@GetMapping(path="/hello-world")
